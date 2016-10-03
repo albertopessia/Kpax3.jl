43359d573a821ad709fea650ddb0c932f62c4950
Alberto Pessia <albertopessia@users.noreply.github.com>
2016-10-03 20:47:52 +0300

    Fix loss_binder function

--- src/model/loss_functions/binder.jl
+++ src/model/loss_functions/binder.jl
@@ -1,14 +1,12 @@
 # This file is part of Kpax3. License is MIT.
 
 function loss_binder(R::Vector{Int},
-                     P::Vector{Float64})
+                     P::Matrix{Float64})
   n = length(R)
 
   loss = 0.0
-  idx = 0
   for j in 1:(n - 1), i in (j + 1):n
-    idx += 1
-    loss += (R[i] == R[j]) ? 1 - P[idx] : P[idx]
+    loss += (R[i] == R[j]) ? 1 - P[i, j] : P[i, j]
   end
 
   loss
