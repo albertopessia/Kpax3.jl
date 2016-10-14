c0facaf8a1740470bacab54c2c89bea93dd18ae2
Alberto Pessia <alberto.pessia@gmail.com>
2016-10-14 09:35:43 +0300

    Add ribbon plot for C

--- src/plots/images.jl
+++ src/plots/images.jl
@@ -116,3 +116,55 @@ function plotP(R::Vector{Int},
        Guide.title(""),
        Guide.colorkey(""))
 end
+
+function plotC(site::Vector{Int},
+               freq::Vector{Float64},
+               C::Matrix{Float64};
+               width::Real=183.0,
+               height::Real=92.0)
+  m = site[end]
+  M = length(site)
+
+  noise = zeros(Float64, m)
+  weak = zeros(Float64, m)
+
+  key = 1
+  for b in 1:M
+    if site[b] != key
+      weak[key] += noise[key]
+      key += 1
+    end
+
+    noise[key] += freq[b] * C[1, b]
+    weak[key] += freq[b] * C[2, b]
+  end
+
+  weak[key] += noise[key]
+
+  # coefficients estimated with a linear model. They can be improved
+  majorfontsize = Measures.Length(:mm, 2 + 0.004 * width + 0.011 * height)
+  minorfontsize = 0.8 * majorfontsize
+  keytitlefontsize = majorfontsize
+  keylabelfontsize = 0.8 * keytitlefontsize
+
+  plot(layer(x=repeat(1:m, outer=3), ymin=[zeros(Float64, m); noise; weak],
+             ymax=[noise; weak; ones(Float64, m)],
+             color=[fill("Noise", m); fill("Weak", m);
+                    fill("Strong", m)], Geom.ribbon),
+       Coord.cartesian(xmin=1, xmax=m, ymin=0, ymax=1),
+       Scale.x_continuous(minvalue=1, maxvalue=m),
+       Scale.y_continuous(minvalue=0, maxvalue=1),
+       Scale.color_discrete_manual(colorant"white", colorant"gray",
+                                   colorant"black"),
+       Theme(default_color=colorant"black", background_color=colorant"white",
+             panel_fill=colorant"white", grid_line_width=0mm,
+             lowlight_color=c->c, lowlight_opacity=0.0,
+             major_label_font_size=majorfontsize,
+             minor_label_font_size=minorfontsize,
+             key_title_font_size=keytitlefontsize,
+             key_label_font_size=keylabelfontsize),
+       Guide.xlabel(""),
+       Guide.ylabel("Posterior probability"),
+       Guide.title(""),
+       Guide.colorkey("Signal"))
+end
